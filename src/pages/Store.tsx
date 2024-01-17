626529ac2df39b23c53a67fe17093566c2046085
mkhn20002000 <manouchehr.khatami@gmail.com>
2024-01-17 20:40:28 +0100

    create StorreItem

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,17 @@
-export function Store(){
-    return <h1>Store</h1>
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import { StoteItem } from "../components/StoreItem";
+export function Store() {
+  return (
+    <>
+      <h1>Store</h1>
+      <Row sm={1} xs={1} md={2} lg={3} xl={4} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoteItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
