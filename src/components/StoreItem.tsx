c6f2086a19a6b1b8f63de6168f4dcc3c6bc64c21
mkhn20002000 <manouchehr.khatami@gmail.com>
2024-01-18 09:48:38 +0100

    create formatCurrency function

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { formatCurrency } from "../utilities/formatCurrency";
 
 type StoreItemProps = {
@@ -8,9 +8,10 @@ type StoreItemProps = {
   imgUrl: string
 }
 export function StoteItem({ id, name, price, imgUrl }: StoreItemProps) {
+  const quantity = 1;
   return (
     <>
-      <Card>
+      <Card className="h-100">
         <Card.Img
           variant="top"
           src={imgUrl}
@@ -22,6 +23,20 @@ export function StoteItem({ id, name, price, imgUrl }: StoreItemProps) {
             <span className="fs-2">{name}</span>
             <span className="ms-2 text-muted">{formatCurrency(price)}</span>
           </Card.Title>
+          <div className="mt-auto">
+            {
+              quantity===0 ? (<Button className="w-100">+ Add To Cart</Button>):(
+              <div className="d-flex flex-column align-items-center" style={{gap:'0.5'}}>
+                <div className="mb-1 d-flex align-items-center justify-content-center" style={{gap:'0.5'}}>
+                  <Button>-</Button>
+                  <div className="fs-3">{quantity}</div>
+                  <Button>+</Button>
+                </div>
+                <Button className="w-50" style={{outline:'none'}}>Remove</Button>
+              </div>
+              )
+            }
+          </div>
         </Card.Body>
       </Card>
     </>
